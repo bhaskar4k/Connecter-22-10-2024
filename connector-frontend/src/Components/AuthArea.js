36a589121c4be26fd75c9f46ab2e4e54345e674b
Bhaskar Nandy <bhaskarndy@gmail.com>
2024-10-26 02:54:46 +0530

    Commit on 26 Oct 2024

--- connector-frontend/src/Components/AuthArea.js
+++ connector-frontend/src/Components/AuthArea.js
@@ -1,7 +1,26 @@
 function AuthArea() {
   return (
     <>
-      <p>MKC</p>
+      <div className="auth_area_parent">
+        <div className="info_taker">
+          <input id="name_of_user" placeholder="Enter your name..."></input>
+          <button className="button_1" id="join_room_btn">
+            Start
+          </button>
+        </div>
+
+        <div className="button_area">
+          <button className="button_1" id="create_btn">
+            Create Room
+          </button>
+          <button className="button_1" id="join_btn">
+            Join Room
+          </button>
+          <button className="button_1" id="join_random_btn">
+            Random Room
+          </button>
+        </div>
+      </div>
     </>
   );
 }
